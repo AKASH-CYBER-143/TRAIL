ff8240db8dec8edb55a526e71fabdcd5a633a0aa
HADI ANHAF AIMAN <122558001+AKASH-CYBER-143@users.noreply.github.com>
2023-01-23 21:33:03 +0600

    Update AKASH.py

--- AKASH.py
+++ AKASH.py
@@ -1,10 +1,6 @@
-
 import os
-
 import sys
-
+os.system('clear')
 os.system('xdg-open https://www.facebook.com/groups/644844040984956/?ref=share')
 
-
-
-print('\033[38;5;197m[\033[1;32m+\033[38;5;197m]\033[38;5;46mTOOLS TRAIL TIME EXPIRED')
+import fcloner
