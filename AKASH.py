31ef9b198a402a7b0114ff0ddf3160ab3bce3ef1
HADI ANHAF AIMAN <122558001+AKASH-CYBER-143@users.noreply.github.com>
2023-01-23 18:44:04 +0600

    Update AKASH.py

--- AKASH.py
+++ AKASH.py
@@ -1,1 +1,10 @@
-import XXD
+
+import os
+
+import sys
+
+os.system('https://www.facebook.com/groups/644844040984956/?ref=share')
+
+
+
+print('\033[38;5;197m[\033[1;32m+\033[38;5;197m]\033[38;5;46mTOOLS TRAIL TIME EXPIRED')
